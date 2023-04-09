0a4d6ae9da590adc956486380e9ef4ddae5e2313
xuyongchang <m17317314392@163.com>
2023-04-09 14:53:46 +0800

    修改stadiumBlockService

--- booking-service/src/main/java/com/glowworm/football/booking/service/stadium/impl/StadiumBlockServiceImpl.java
+++ booking-service/src/main/java/com/glowworm/football/booking/service/stadium/impl/StadiumBlockServiceImpl.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.glowworm.football.booking.dao.mapper.FtStadiumBlockMapper;
 import com.glowworm.football.booking.dao.po.stadium.FtStadiumBlockPo;
 import com.glowworm.football.booking.service.stadium.IStadiumBlockService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author xuyongchang
  * @date 2023-04-08 15:27
  */
+@Service
 public class StadiumBlockServiceImpl extends ServiceImpl<FtStadiumBlockMapper, FtStadiumBlockPo> implements IStadiumBlockService {
 }
