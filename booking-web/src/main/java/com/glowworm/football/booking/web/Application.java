657a9092669f42da119f6e52d4e2bb771be447d3
xuyongchang <m17317314392@163.com>
2023-04-10 18:41:42 +0800

    修改时区

--- booking-web/src/main/java/com/glowworm/football/booking/web/Application.java
+++ booking-web/src/main/java/com/glowworm/football/booking/web/Application.java
@@ -3,6 +3,9 @@ package com.glowworm.football.booking.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 /**
  * @author xuyongchang
  * @date 2023/3/16
@@ -11,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.SHORT_IDS.get("CTT")));
 
         SpringApplication.run(Application.class, args);
     }
